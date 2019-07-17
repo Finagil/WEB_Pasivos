df4d33aa5a9da1c7bae63d2899065dd072f4d080
ecacerest <ecacerest@finagil.com.mx>
2019-07-17 14:17:47 -0500

    se quito configuracion SMTP se manda correos via CorreosSeguimiento

--- Globales.vb
+++ Globales.vb
@@ -1,6 +1,4 @@
-Imports System.Net.Mail
 Module Globales
-
     Public Function CalculaDias() As Integer
         Dim fecha_inicial As Date = Date.Now
         Dim fecha_final As Date = "31/12/" & Date.Now.Year.ToString
@@ -25,41 +23,11 @@ Module Globales
         Return (dias)
     End Function
 
-    'Public Sub MandaCorreo(ByVal id As Integer, ByVal Notas As String)
-    '    Dim tp As New ProductionDSTableAdapters.PromotoresTableAdapter
-    '    Dim ts As New ProductionDSTableAdapters.PROM_SeguimientoTableAdapter
-    '    Dim tts As New ProductionDS.PROM_SeguimientoDataTable
-    '    Dim rr As ProductionDS.PROM_SeguimientoRow
-    '    ts.FillByID(tts, id)
-    '    rr = tts.Rows(0)
-    '    Dim para As String = tp.SacaCorreo(rr.Promotor)
-    '    Dim Mensaje As String = ""
-    '    Mensaje += "<b>Asignación de prospecto:</b> " & rr.NombreProspecto & "<br>"
-    '    Mensaje += "<b>Fecha :</b> " & rr.Fecha.ToShortDateString & "<br>"
-    '    Mensaje += "<b>Notas :</b> " & Notas & "<br>"
-    '    EnviaCorreo(para, Mensaje, "Asignación de prospecto: " & rr.NombreProspecto)
-    'End Sub
-
     Public Sub EnviaCorreo(ByVal De As String, ByVal Para As String, ByVal Mensaje As String, ByVal Asunto As String)
-        Dim Mensage As New MailMessage(De, Trim(Para), Trim(Asunto), Mensaje)
-        Dim Cliente As New SmtpClient(My.Settings.SmtpSRV, My.Settings.SmtpPORT)
-        Dim Credenciales As String() = My.Settings.SmtpCreden.Split(",")
-        Cliente.Credentials = New System.Net.NetworkCredential(Credenciales(0), Credenciales(1), Credenciales(2))
-        Mensage.IsBodyHtml = True
-        Cliente.Send(Mensage)
+        Dim taCorreos As New WEB_FinagilDSTableAdapters.GEN_Correos_SistemaFinagilTableAdapter
+        taCorreos.Insert(De, Para, Asunto, Mensaje, False, Date.Now, "")
+        taCorreos.Dispose()
     End Sub
-
-    'Public Sub CargaFacturas()
-    '    Dim Baan As New BaanDSTableAdapters.ttfacr962200TableAdapter
-    '    Dim Tbaan As New BaanDS.ttfacr962200DataTable
-    '    Dim ta As New WEB_FinagilDSTableAdapters.WEB_FacturasTableAdapter
-    '    Baan.Fill(Tbaan, Date.Now.AddDays(-40))
-    '    For Each r As BaanDS.ttfacr962200Row In Tbaan.Rows
-    '        If ta.ExisteFactura(r.factura) <= 0 Then
-    '            ta.InsertNuevo(r.factura.Trim, r.rfc.Trim, r.importe, r.fechaproceso, r.fechavencimiento)
-    '        End If
-    '    Next
-    'End Sub
 
     Public Function EncuentraBaseFOR(Capital As Decimal, Tasa As Decimal, Rete As Decimal, Incre As Decimal) As Decimal
         Dim AuxRete As Decimal
--- My Project/Settings.Designer.vb
+++ My Project/Settings.Designer.vb
@@ -15,7 +15,7 @@ Option Explicit On
 Namespace My
     
     <Global.System.Runtime.CompilerServices.CompilerGeneratedAttribute(),  _
-     Global.System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "15.8.0.0"),  _
+     Global.System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "15.9.0.0"),  _
      Global.System.ComponentModel.EditorBrowsableAttribute(Global.System.ComponentModel.EditorBrowsableState.Advanced)>  _
     Partial Friend NotInheritable Class MySettings
         Inherits Global.System.Configuration.ApplicationSettingsBase
@@ -51,24 +51,6 @@ Namespace My
                 End If
 #End If
                 Return defaultInstance
-            End Get
-        End Property
-        
-        <Global.System.Configuration.ApplicationScopedSettingAttribute(),  _
-         Global.System.Diagnostics.DebuggerNonUserCodeAttribute(),  _
-         Global.System.Configuration.DefaultSettingValueAttribute("smtp85.cmoderna.com")>  _
-        Public ReadOnly Property SmtpSRV() As String
-            Get
-                Return CType(Me("SmtpSRV"),String)
-            End Get
-        End Property
-        
-        <Global.System.Configuration.ApplicationScopedSettingAttribute(),  _
-         Global.System.Diagnostics.DebuggerNonUserCodeAttribute(),  _
-         Global.System.Configuration.DefaultSettingValueAttribute("26")>  _
-        Public ReadOnly Property SmtpPORT() As String
-            Get
-                Return CType(Me("SmtpPORT"),String)
             End Get
         End Property
         
@@ -125,15 +107,6 @@ Namespace My
         Public ReadOnly Property WEB_FinagilConnectionString() As String
             Get
                 Return CType(Me("WEB_FinagilConnectionString"),String)
-            End Get
-        End Property
-        
-        <Global.System.Configuration.ApplicationScopedSettingAttribute(),  _
-         Global.System.Diagnostics.DebuggerNonUserCodeAttribute(),  _
-         Global.System.Configuration.DefaultSettingValueAttribute("ecacerest,c4c3r1t0s,cmoderna")>  _
-        Public ReadOnly Property SmtpCreden() As String
-            Get
-                Return CType(Me("SmtpCreden"),String)
             End Get
         End Property
     End Class
